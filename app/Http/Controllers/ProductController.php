33299fb9205528cec1af53b769a60902f1f60a6f
Md Feroj Bepari <feroj21@gmail.com>
2021-08-07 11:33:47 +0600

    Product resrouce added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function store()
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
     {
+        $products = Product::all();
 
+        return response()->json([
+            'data' => $products,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|string',
+            'category_id' => 'required|exists:category,id',
+        ]);
+
+        $product = Product::create([
+            'name' => $validatedData['name'],
+            'type' => $validatedData['type'],
+            'category_id' => $validatedData['category_id'],
+        ]);
+
+        return response()->json([
+            'data' => $product
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param Product $product
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|string',
+            'category_id' => 'required|exists:category,id',
+        ]);
+
+        $product->name = $validatedData['name'];
+        $product->type = $validatedData['type'];
+        $product->category_id = $validatedData['category_id'];
+
+        $product->save();
+
+        return response()->json([
+            'data' => $product
+        ]);
     }
 }
--- database/migrations/2021_07_30_063442_create_products_table.php
+++ database/migrations/2021_07_30_063442_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
         });
     }
